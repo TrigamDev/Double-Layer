195865ac7236c3f857938eb6f0c919d4609d591d
Trigam <trigambusiness@gmail.com>
2023-09-11 16:24:35 -0400

    Animation fixes
    - Fixed the idle animation always pointing down
    - Fixed the animation not appearing to change if you press movement keys to quickly

--- scripts/entities/player.gd
+++ scripts/entities/player.gd
@@ -4,9 +4,14 @@ extends CharacterBody2D
 @export var run_speed = 450
 @export var acceleration = 3500
 
+
 enum move_state {
 	IDLE, WALK, RUN, ROLL
 }
+enum move_direction {
+	UP, DOWN, LEFT, RIGHT, NONE
+}
+var movement_direction = move_direction.NONE
 var movement_state: move_state = move_state.IDLE
 
 func _physics_process(delta):
@@ -28,8 +33,25 @@ func _physics_process(delta):
 	else: movement_state = move_state.WALK
 
 func set_animation(direction: Vector2):
-	if (direction.x > 0): $Sprites.play("walk_right")
-	elif (direction.x < 0): $Sprites.play("walk_left")
-	elif (direction.y > 0): $Sprites.play("walk_down")
-	elif (direction.y < 0): $Sprites.play("walk_up")
-	else: $Sprites.play("idle")
+	if (direction.x > 0):
+		$Sprites.play("walk_right")
+		movement_direction = move_direction.RIGHT
+	elif (direction.x < 0):
+		$Sprites.play("walk_left")
+		movement_direction = move_direction.LEFT
+	elif (direction.y > 0):
+		$Sprites.play("walk_down")
+		movement_direction = move_direction.DOWN
+	elif (direction.y < 0):
+		$Sprites.play("walk_up")
+		movement_direction = move_direction.UP
+	else: set_idle_anim()
+
+func set_idle_anim():
+	if (movement_direction == move_direction.UP):
+		$Sprites.play("idle_up")
+	elif (movement_direction == move_direction.LEFT):
+		$Sprites.play("idle_left")
+	elif (movement_direction == move_direction.RIGHT):
+		$Sprites.play("idle_right")
+	else: $Sprites.play("idle_down")
